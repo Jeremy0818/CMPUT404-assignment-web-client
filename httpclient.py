3703a678af38a2e76165458c47c3d4685342eb2c
Jeremy0818 <sheeyeowchoo@gmail.com>
2021-02-09 18:49:00 -0500

    added function headers

--- httpclient.py
+++ httpclient.py
@@ -35,6 +35,15 @@ class HTTPResponse(object):
 class HTTPClient(object):
     
     def handle_args(self, args):
+        """
+    	This function will get the dictionary arguments and convert it
+    	into application/x-www-form-urlencoded content type.
+
+    	Argument:
+    		args: a dictionary that contains the parameters for posting
+    	Return:
+    		the parameters in the form of application/x-www-form-urlencoded
+    	"""
         params = ""
         if args is None: return params
         for k, v in args.items():
@@ -42,11 +51,22 @@ class HTTPClient(object):
         return params[:-1]
 
     def get_host_port_path(self,url):
+        """
+    	This function will get the host, port, and path from the url
+    	using urlparse in urllib.parse library as a helper function.
+
+    	Argument:
+    		url: a URL string for the request
+    	Return:
+    		host: host of the server
+    		port: port for connecting the server
+    		path: the specific path of the request
+    	"""
         o = urllib.parse.urlparse(url)
         host = o.netloc.split(':')[0]
         port = o.port
         if port is None:
-            port = 80 # default port number is 80
+            port = 80  # default port number is 80
         path = o.path
         if len(path) == 0:
             path = "/"
@@ -59,15 +79,43 @@ class HTTPClient(object):
         return None
 
     def get_code(self, data):
-        lines = self.get_headers(data)
-        code = int(lines[0].split(' ')[1])
+        """
+    	This function will call the get_headers function to get the
+    	header from the data returned from the server, and then get
+    	the status code indicated in the header.
+
+    	Argument:
+    		data: a string of the response from the server
+    	Return:
+    		code: status code indicated in the data
+    	"""
+        header = self.get_headers(data)
+        code = int(header.split(' ')[1])
         print("Status code: ", code)
         return code
 
     def get_headers(self,data):
-        return data.split('\r\n')
+        """
+    	This function will get the header from the data returned from the
+    	server and return the header.
+
+    	Argument:
+    		data: a string of the response from the server
+    	Return:
+    		header: header of the HTTP response
+    	"""
+        return data.split('\r\n')[0]
 
     def get_body(self, data):
+        """
+    	This function will get the body from the data returned from the
+    	server and return the header.
+
+    	Argument:
+    		data: a string of the response from the server
+    	Return:
+    		header: body of the HTTP response
+    	"""
         return data.split('\r\n\r\n')[1]
     
     def sendall(self, data):
@@ -89,6 +137,26 @@ class HTTPClient(object):
         return buffer.decode('utf-8')
 
     def GET(self, url, args=None):
+        """
+        This function will first try to get the host, port and path
+        in the url. Then, it will connect the server by calling the
+        connect helper function. After that, it will generate a GET
+        request and send it to the server by calling sendall. Finally,
+        It will call the recvall function to read the response from
+        the server and return the reponse as an HTTPResponse object
+        with the status code and body of the response.
+
+        Argument:
+            url: a string of the request
+            args: None (Not handled)
+        Return:
+            HTTPResponse: an object of HTTPResponse
+                If successful, it returns with the status code and 
+                body of the response from the server.
+                If it fails to make the request or get a proper
+                response it will return 404 as the status code with
+                an empty body.
+        """
         print("\n--- Making GET request ---\n")
         try:
             host, port, path = self.get_host_port_path(url)
@@ -116,6 +184,28 @@ class HTTPClient(object):
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
+        """
+        This function will first try to handle the arguments so that
+        the data is converted to the correct format. Then, it will
+        get the host, port and path in the url. Then, it will connect 
+        the server by calling the connect helper function. After that, 
+        it will generate a POST request and send it to the server by 
+        calling sendall. Finally, it will call the recvall function 
+        to read the response from the server and return the reponse 
+        as an HTTPResponse object with the status code and body of the 
+        response.
+
+        Argument:
+            url: a string of the request
+            args: parameters to be posted
+        Return:
+            HTTPResponse: an object of HTTPResponse
+                If successful, it returns with the status code and 
+                body of the response from the server.
+                If it fails to make the request or get a proper
+                response it will return 404 as the status code with
+                an empty body.
+        """
         print("\n--- Making POST request ---\n")
         try:
             params = self.handle_args(args)
